fea024f4729697f820135bf0b7a6c11287c6ff35
Pedro Henrique Vieira dos Santos <pedrohenriquevieiradossantos1@gmail.com>
2025-07-01 11:19:10 -0300

    Tela principal adicionada, tabela criada, classes filhas de Profissionais criadas

---
+++ grupo8-main/src/main/java/Classes/Acoes.java
@@ -0,0 +1,26 @@
+package Classes;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Acoes
+{
+
+    @FXML
+    public void pesquisar() throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/TelaPrincipal.fxml"));
+        Parent logar = fxmlLoader.load();
+
+        Stage novaJanela = new Stage();
+        novaJanela.setTitle("SpaFy");
+        novaJanela.setScene(new Scene(logar));
+        novaJanela.show();
+    }
+
+}
